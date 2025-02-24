6c642063af1bbc39d94e312327dee92e973274ee
pedromarckez <pedromarquez93@gmail.com>
2025-02-24 21:44:41 +0100

    main.js

--- src/main.js
+++ src/main.js
@@ -15,24 +15,24 @@ app.append(sectionCards);
 
 cards.forEach((card, index) => sectionCards.appendChild(Card(card, index)));
 
-// Function to reset to initial state
+
 const resetToInitialState = () => {
     sectionCards.innerHTML = '';
     cards.forEach((card, index) => sectionCards.appendChild(Card(card, index)));
 }
 
-// Ensure DOM is fully loaded before adding event listener
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector(".logo").addEventListener('click', resetToInitialState);
 
-    // Add event listener for search results
+    
     window.addEventListener('search', (event) => {
         const results = event.detail;
 
-        // Filter cards based on search results
+      
         const filteredCards = cards.filter(card => results.some(result => result.key === card.clave));
 
-        // Update sectionCards with filtered cards
+        
         sectionCards.innerHTML = '';
         filteredCards.forEach((card, index) => sectionCards.appendChild(Card(card, index)));
     });
